00823fda01b52d12a067ae405a5c4f8778b0057b
Cyranno973 <gs.gouin@gmail.com>
2019-12-27 11:30:42 +0100

    menu prenant toute la page

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var menu = document.getElementById('menu-toggle');
+var nav = document.getElementById('menu-overlay');
+
+menu.addEventListener('click', function(){
+ nav.classList.toggle('active');
+
+ menu.classList.toggle('active');
+
+})
